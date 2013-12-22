742b7d18731da3fa9e8b5be306076fdd647e3c28
J. Tao <jon.g.tao@gmail.com>
2013-12-21 16:38:11 -0800

    added working example

--- shadowlib/shadowlib.c
+++ shadowlib/shadowlib.c
@@ -1,1 +1,20 @@
 // Wrapper for shadow generator
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+static int ctrig_shadow(lua_State *L)
+{
+	printf("asdf\n");
+	return 0;
+} // trig_shadow
+
+
+
+
+int luaopen_shadowlib(lua_State *L)
+{
+	lua_register(L, "ctrig_shadow", ctrig_shadow);
+	return 0;
+} // luaopen_power()
--- shadowlib/test.lua
+++ shadowlib/test.lua
@@ -1,1 +1,5 @@
 -- Lua test for shadow library
+
+require("shadowlib")
+ctrig_shadow()
+
